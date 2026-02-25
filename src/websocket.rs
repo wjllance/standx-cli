8e8f458b10736efe5a0217b59ffbe77525094faa
Junlin <junlin@standx.com>
2026-02-26 00:27:00 +0800

    Revert "feat: add Bearer prefix to WebSocket auth token"
    This reverts commit 854b300e980be79f23141d9a86b9653cfccdc5b7.

--- src/websocket.rs
+++ src/websocket.rs
@@ -243,7 +243,7 @@ async fn connect_and_run(
 
         let auth_msg = serde_json::json!({
             "auth": {
-                "token": format!("Bearer {}", t),
+                "token": t,
                 "streams": streams
             }
         });
